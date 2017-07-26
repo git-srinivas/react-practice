4d3b404c31f6a82752e4f7d261420cb3521253a4
srinivas vykuntam <srinivas@srinivass-MacBook-Pro.local>
2017-07-26 23:37:55 +0800

    added update to do

--- src/lib/todoHelpers.js
+++ src/lib/todoHelpers.js
@@ -1,3 +1,16 @@
 export const addTodo = (list,item) => [...list,item]
 
 export const generateId = () => Math.floor(Math.random()*100000)
+
+export const findById = (id,list) => list.find(item => item.id === id)
+
+export const toggleTodo = (item) => ({...item,isComplete:!item.isComplete})
+
+export const updateTodo = (update,list) => {
+  const itemIndex = list.findIndex(item => item.id === update.id);
+  return [
+    ...list.slice(0,itemIndex),
+    update,
+    ...list.slice(itemIndex+1)
+  ]
+}
